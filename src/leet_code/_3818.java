435d813ad845960ad343bbd4ab897505d28c6ecc
GodOfTruth <6386391ritesh@gmail.com>
2026-04-14 10:13:31 +0530

    new code

--- src/leet_code/_3818.java
+++ src/leet_code/_3818.java
@@ -1,2 +1,19 @@
-package leet_code;public class _3818 {
+package leet_code;
+
+public class _3818 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[]{1, -1, 2, 3, 3, 4, 5};
+		System.out.println(minimumPrefixLength(arr));
+	}
+
+	public static int minimumPrefixLength(int[] nums) {
+		int i = nums.length - 1;
+
+		while (i > 0 && nums[i - 1] < nums[i]) {
+			i--;
+		}
+
+		return i;
+	}
 }
